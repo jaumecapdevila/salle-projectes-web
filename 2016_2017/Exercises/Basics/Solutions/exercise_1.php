4ec9cad3d0c2d5b45fb9207894cef98189dbaf18
Jaume Capdevila Pérez <contact@jaumecapdevila.net>
2018-02-06 17:28:25 +0100

    Removed solutions

--- 2016_2017/Exercises/Basics/Solutions/exercise_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($argc != 2) {
-    echo "Usage: php exercise_1.php [number] \n";
-    exit(1);
-}
-$number = $argv[1];
-for ($i=0; $i <= 10; $i++) {
-    echo $i*$number . "\n";
-}
--- 2016_2017/Exercises/Basics/Solutions/exercise_6/display_completed_tasks.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = file_get_contents(__DIR__ . "/completed.txt");
-$tasks = explode(",", $file);
-foreach ($tasks as $key => $task) {
-    echo nl2br("Task number $key: $task \n");
-}
